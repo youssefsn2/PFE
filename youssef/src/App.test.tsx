0eb97e2e772e102c0ab805cfb2f775568e2643b4
youssefsn2 <youssefserhane7@gmail.com>
2025-06-14 13:47:49 +0200

    fix(frontend): Wrap App component in router for tests

--- youssef/src/App.test.tsx
+++ youssef/src/App.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
 
 test('renders learn react link', () => {
   render(<App />);
